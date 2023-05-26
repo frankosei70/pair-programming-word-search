e0c1ee902db278af06b19e72999d90bb62b8cbeb
frank <frankosei70@yahoo.com>
2023-05-26 02:01:01 -0700

    corrected to pass second test

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,28 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    if (letters.length === 0) {
+      return false; // Return false for empty matrix
     }
-}
+  
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (const l of horizontalJoin) {
+      if (l.includes(word)) {
+        return true; // Return true for horizontal word search
+      }
+    }
+  
+    const verticalJoin = transpose(letters).map(ls => ls.join(''));
+    for (const l of verticalJoin) {
+      if (l.includes(word)) {
+        return true; // Return true for vertical word search
+      }
+    }
+  
+    return false;
+  }
+  
+  
+  const transpose = matrix => matrix[0].map((_, i) => matrix.map(row => row[i]));
+  
+  
 
 module.exports = wordSearch
